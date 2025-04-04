211b899fc6219fdd070b50674dfbb0e64bfbf471
wyndham <126304806+blaxokreemo@users.noreply.github.com>
2025-04-04 12:44:18 -0400

    Added background image to header. Created acf-json directory and added json file for performance fields.

--- wp-content/themes/vsc-website/header.php
+++ wp-content/themes/vsc-website/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-<header class="site-header">
+<header class="site-header" style="background-image: url('<?php echo get_theme_file_uri('images/header2.jpg'); ?>')">
     <h1 class="site-title-text"><a href="<?php echo site_url(); ?>">Vermont Suitcase Company</a></h1>
     <!-- <nav class="site-nav">
         <ul>
