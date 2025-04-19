19047671a40a2ab01718e0136512527170636194
wyndham <126304806+blaxokreemo@users.noreply.github.com>
2025-04-19 18:42:55 -0400

    Reworked footer and added social icons

--- wp-content/themes/vsc-website/footer.php
+++ wp-content/themes/vsc-website/footer.php
@@ -5,6 +5,11 @@
 <div class="content-box footer-button">
     <a href="mailto:vermontsuitcasecompany@gmail.com"><h1 class="contact-us">Contact Us</h1></a>
 </div>
+<div class="social-media">
+        <a href="https://www.instagram.com/vermontsuitcasecompany/" target="_blank"><i class="fa-brands fa-instagram social"></i></a>
+        <a href="https://www.facebook.com/VermontSuitcaseCompany" target="_blank"><i class="fa-brands fa-facebook social"></i></a>
+        <a href="https://www.youtube.com/@@vermontsuitcasecompany7681" target="_blank"><i class="fa-brands fa-youtube social"></i></a>
+</div>
 
 
 </footer>
