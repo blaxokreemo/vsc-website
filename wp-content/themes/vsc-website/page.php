b310ca3d67ab9d03bd9f4273cc2fded9853830b5
wyndham <126304806+blaxokreemo@users.noreply.github.com>
2025-08-10 20:26:01 -0400

    Added conditional php to add or remove room for side menu depending on if page had parents or children

--- wp-content/themes/vsc-website/page.php
+++ wp-content/themes/vsc-website/page.php
@@ -1,13 +1,23 @@
 <?php get_header(); 
+
+$theParent = wp_get_post_parent_id(get_the_ID());
+            $test_array = get_pages(array(
+                'child_of' => get_the_ID()
+            ));
+            
+            if ($theParent or $test_array) {
 ?>
 
 <div class="container-flex content-narrow room-for-menu">
 
     <?php
   get_template_part('template-parts/side-menu');
-    ?>
-
-    <?php while(have_posts()) {
+            }
+    else { ?>
+<div class="container-flex content-narrow">
+    <?php }
+    
+    while(have_posts()) {
       the_post(); ?>
     <div class="page-contents">
         <div class="content-box box-header">
