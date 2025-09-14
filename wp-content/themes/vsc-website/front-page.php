65ca28aed3ef3f69803d7112710cdc8d05283650
wyndham <126304806+blaxokreemo@users.noreply.github.com>
2025-09-14 14:14:49 -0400

    Updated front page to display information about MONIFF

--- wp-content/themes/vsc-website/front-page.php
+++ wp-content/themes/vsc-website/front-page.php
@@ -9,26 +9,29 @@
 <div class="content-narrow container-flex full-page-height">
   <div class="tour-dates">
     <div class="content-box tour-header">
-      <h1 class="tour-header">New Clothes Wins Two Awards at Vermont Film Festival!</h1>
+    <img src="<?php echo get_theme_file_uri('/images/moniff.png') ?>" alt="logo for Monadnock International Film Festival" class="moniff-logo">  
+    <h1 class="tour-header">See New Clothes at the Monadnock International Film Festival on 10/5</h1>
+      
     </div>
+    
     <div class="content-box performance-container">
         <h1 class="tour-title">New Clothes</h1>
-        <h2 class="tour-subtitle">Best Narrative Feature</h2>
-        <h2 class="tour-subtitle">Best Emerging Filmmakers</h2>
+        <h2 class="tour-subtitle">Sunday, 10/5 at 12:30 p.m.</h2>
+        <h1 class="tour-title">Birdies</h2>
+        <h2 class="tour-subtitle">18 Main Street in Keene, NH</h2>
         <div class="content-box the-content">
-          <p>We had a great time at <a href="https://vermontfilmfestival.com" target="_blank">Vermont Film Festival</a> and met a lot of wonderful
-            artists. Our film <i><a href="https://newclothesmovie.com" target="_blank">New Clothes</a></i>
-            won two awards! We're hoping to take it on a tour of Vermont within the next year. If you'd like to stay
-            up to date on our schedule, make sure to join our <strong class="mailing-list-link">mailing list</strong>!
-          </p>
-          </p>
+          <p>We're so excited to be a part of the <a href="https://moniff.org">Monadnock International Film Festival</a>
+            this year! We'll be screening our film <i><a href="https://newclothesmovie.com" target="_blank">New Clothes</a></i> on Sunday, October 5th at 12:30 p.m. at
+            Birdies in Keene, NH. <a href="https://moniff2025.eventive.org/schedule/new-clothes-68b8d0e076ddd4f3a32e63bb">Get your tickets here.</a>
+            We hope to see you there!</p>
+          <p>As always, if you'd like to stay up to date with our schedule, make sure to join our <strong class="mailing-list-link">mailing list</strong>!</p>
         </div>
           
     </div>
   </div>
 
   <!-- <div class="poster-box content-box"> -->
-    <img src="<?php echo get_theme_file_uri('/images/newclothes.jpg') ?>" alt="poster for The Duke Is Dead" class="poster-image">
+    <img src="<?php echo get_theme_file_uri('/images/newclothes.jpg') ?>" alt="poster for New Clothes" class="poster-image">
   <!-- </div> -->
   
   <div class="video">
