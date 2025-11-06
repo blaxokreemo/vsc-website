65860c9d7090c52bcc2fc16770a39470cc710d30
wyndham <126304806+blaxokreemo@users.noreply.github.com>
2025-11-05 22:38:02 -0600

    Changed front page back to schedule for winter tour

---
+++ wp-content/themes/vsc-website/ANNOUNCEMENT-front-page.php
@@ -0,0 +1,49 @@
+<?php get_header(); ?>
+
+<div class="container-flex-no-padding content-narrow mailing-list-button">
+  <button id="mailing-list-button">Join Our Mailing List</button>
+</div>
+
+
+
+<div class="content-narrow container-flex full-page-height">
+  <div class="tour-dates">
+    <div class="content-box tour-header">
+    <img src="<?php echo get_theme_file_uri('/images/moniff.png') ?>" alt="logo for Monadnock International Film Festival" class="moniff-logo">  
+    <h1 class="tour-header">See New Clothes at the Monadnock International Film Festival on 10/5</h1>
+      
+    </div>
+    
+    <div class="content-box performance-container">
+        <h1 class="tour-title">New Clothes</h1>
+        <h2 class="tour-subtitle">Sunday, 10/5 at 12:30 p.m.</h2>
+        <h1 class="tour-title">Birdies</h2>
+        <h2 class="tour-subtitle">18 Main Street in Keene, NH</h2>
+        <div class="content-box the-content">
+          <p>We're so excited to be a part of the <a href="https://moniff.org">Monadnock International Film Festival</a>
+            this year! We'll be screening our film <i><a href="https://newclothesmovie.com" target="_blank">New Clothes</a></i> on Sunday, October 5th at 12:30 p.m. at
+            Birdies in Keene, NH. <a href="https://moniff2025.eventive.org/schedule/new-clothes-68b8d0e076ddd4f3a32e63bb">Get your tickets here.</a>
+            We hope to see you there!</p>
+          <p>As always, if you'd like to stay up to date with our schedule, make sure to join our <strong class="mailing-list-link">mailing list</strong>!</p>
+        </div>
+          
+    </div>
+  </div>
+
+  <!-- <div class="poster-box content-box"> -->
+    <img src="<?php echo get_theme_file_uri('/images/newclothes.jpg') ?>" alt="poster for New Clothes" class="poster-image">
+  <!-- </div> -->
+  
+  <div class="video">
+    <div class="content-box">
+        <h2 class="video-title">What is Vermont Suitcase Company?</h2>
+    </div>
+    <div class="video-box">
+    <iframe title="vimeo-player" src="https://player.vimeo.com/video/1066445073?h=c030265f72" width="640" height="360" frameborder="0"    allowfullscreen></iframe>
+  </div>
+
+</div>
+
+
+
+<?php get_footer(); ?>
--- wp-content/themes/vsc-website/front-page.php
+++ wp-content/themes/vsc-website/front-page.php
@@ -1,37 +1,82 @@
 <?php get_header(); ?>
 
-<div class="container-flex-no-padding content-narrow mailing-list-button">
-  <button id="mailing-list-button">Join Our Mailing List</button>
-</div>
-
-
-
 <div class="content-narrow container-flex full-page-height">
   <div class="tour-dates">
     <div class="content-box tour-header">
-    <img src="<?php echo get_theme_file_uri('/images/moniff.png') ?>" alt="logo for Monadnock International Film Festival" class="moniff-logo">  
-    <h1 class="tour-header">See New Clothes at the Monadnock International Film Festival on 10/5</h1>
-      
+      <h1 class="tour-header">Winter 2025 Tour Dates</h1>
     </div>
-    
     <div class="content-box performance-container">
-        <h1 class="tour-title">New Clothes</h1>
-        <h2 class="tour-subtitle">Sunday, 10/5 at 12:30 p.m.</h2>
-        <h1 class="tour-title">Birdies</h2>
-        <h2 class="tour-subtitle">18 Main Street in Keene, NH</h2>
-        <div class="content-box the-content">
-          <p>We're so excited to be a part of the <a href="https://moniff.org">Monadnock International Film Festival</a>
-            this year! We'll be screening our film <i><a href="https://newclothesmovie.com" target="_blank">New Clothes</a></i> on Sunday, October 5th at 12:30 p.m. at
-            Birdies in Keene, NH. <a href="https://moniff2025.eventive.org/schedule/new-clothes-68b8d0e076ddd4f3a32e63bb">Get your tickets here.</a>
-            We hope to see you there!</p>
-          <p>As always, if you'd like to stay up to date with our schedule, make sure to join our <strong class="mailing-list-link">mailing list</strong>!</p>
-        </div>
+        <h1 class="tour-title">King Wenceslas</h1>
+        <h2 class="tour-subtitle">A Vermont Suitcase Holiday Show</h2>
+        <div class="performance-table">
+          <?php
+          $today = date('Ymd');
+          $homepagePerformances = new WP_Query(
+            array(
+              'posts_per_page' => -1,
+              'post_type' => 'performance',
+              'orderby' => 'meta_value_num',
+              'order' => 'ASC',
+              'meta_key' => 'date',
+              'meta_query' => array(
+                array(
+                  'key' => 'date',
+                  'compare' => '>=',
+                  'value' => $today,
+                  'type' => 'numeric'
+                )
+              )
+            )
+          );
+
+          while($homepagePerformances->have_posts()) {
+            $homepagePerformances->the_post(); 
+          
+            ?>
+            <a href="<?php the_permalink(); ?>" class="performance-link">
+            <div class="performance-entry">
+              <div class="performance-date">
+                <div class="date">
+                <?php 
+                  $date = get_field('date');
+                  $formattedDate = date('l, F j', strtotime($date));
+                  echo $formattedDate; ?>
+                </div>
+                <div class="time">
+                <?php
+                  $time = get_field('time');
+                  echo $time;  
+                ?>
+                </div>
+              </div>  
+              <div class="performance-location">
+                <div class="venue">
+                <?php 
+                  $venue = get_field('venue');
+                  echo $venue; 
+                ?>
+                </div>
+                <div class="address">
+                <?php 
+                  $address = get_field('address');
+                  echo $address;
+                ?>
+                </div>
+              </div>
+            </div>
+            </a>
+
+          <?php 
+          }
+          ?>
+
+      </div>
           
     </div>
   </div>
 
   <!-- <div class="poster-box content-box"> -->
-    <img src="<?php echo get_theme_file_uri('/images/newclothes.jpg') ?>" alt="poster for New Clothes" class="poster-image">
+    <img src="<?php echo get_theme_file_uri('/images/poster.jpg') ?>" alt="poster for The Duke Is Dead" class="poster-image">
   <!-- </div> -->
   
   <div class="video">
