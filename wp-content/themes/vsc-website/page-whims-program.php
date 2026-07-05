3c576891b7e4c8654fb1ef3eafd52b7971cfdead
wyndham <126304806+blaxokreemo@users.noreply.github.com>
2026-07-05 14:06:42 +0000

    Made program for My Lady of Whims

---
+++ wp-content/themes/vsc-website/page-whims-program.php
@@ -0,0 +1,100 @@
+<?php get_header(); ?>
+
+<div class="container-flex">
+  <div class="program">
+    <div class="content-box tour-header">
+      <h1 class="tour-header">My Lady of Whims</h1>
+    </div>
+    <div class="content-box">
+      <div class="container-flex-no-padding program-section">
+        <div class="flex-picture"><img src="<?php echo get_theme_file_uri('/images/whims-poster.jpg') ?>" alt="poster for My Lady of Whims" class="poster-image"></div>
+        <div class="flex-description">
+          <p class="show-description text-secondary">With his annual fundraising gala approaching, where is the councilman’s daughter? 
+          If the papers find out she’s playing “Shipyard Sally” with those bohemians down by the docks, his campaign is finished! 
+          Will Otto, the hired gun, rescue her in time? Will Louisa the maid claim the reward for herself? Can they do it before 
+          the banker demolishes their historic hotel headquarters?</p>
+          <div class="creative-team">
+            <div class="creative-credit">
+              <img src="<?php echo get_theme_file_uri('/images/jonny-headshot.jpg') ?>" alt="Jonny Flood" class="creative-photo">
+              <h3 class="creative-credit-role">Written by</h3>
+              <h3 class="creative-credit-name">Jonny Flood</h3>
+            </div>
+            <div class="creative-credit">
+              <img src="<?php echo get_theme_file_uri('/images/wyndham-headshot.jpg') ?>" alt="Wyndham Maxwell" class="creative-photo">
+              <h3 class="creative-credit-role">Directed by</h3>
+              <h3 class="creative-credit-name">Wyndham Maxwell</h3>
+            </div>
+            <div class="creative-credit">
+              <img src="<?php echo get_theme_file_uri('/images/sandy-headshot.jpg') ?>" alt="Jonny Flood" class="creative-photo">
+              <h3 class="creative-credit-role">Production Design by</h3>
+              <h3 class="creative-credit-name">Sandy Klein</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="content-box tour-header">
+        <h1 class="tour-header">Cast</h1>
+      </div>
+      <div class="program-section credits-grid">
+        <div class="credit">
+          <img src="<?php echo get_theme_file_uri('/images/dory-cast-photo.jpg') ?>" alt="Doran Hamm" class="credit-photo">
+          <h3 class="credit-name">Doran Hamm</h3>
+          <h3 class="credit-role">Leo, Rolfe, Conrade</h3>
+        </div>
+        <div class="credit">
+          <img src="<?php echo get_theme_file_uri('/images/izabel-cast-photo.jpg') ?>" alt="Izabel Hendrin" class="credit-photo">
+          <h3 class="credit-name">Izabel Hendrin</h3>
+          <h3 class="credit-role">Minnie</h3>
+        </div>
+        <div class="credit">
+          <img src="<?php echo get_theme_file_uri('/images/aiden-cast-photo.jpg') ?>" alt="Aiden Meyer" class="credit-photo">
+          <h3 class="credit-name">Aiden Meyer</h3>
+          <h3 class="credit-role">Big Eddie Shaw</h3>
+        </div>
+        <div class="credit">
+          <img src="<?php echo get_theme_file_uri('/images/julia-cast-photo.jpg') ?>" alt="Doran Hamm" class="credit-photo">
+          <h3 class="credit-name">Julia Tadlock</h3>
+          <h3 class="credit-role">Louisa</h3>
+        </div>
+        <div class="credit">
+          <img src="<?php echo get_theme_file_uri('/images/shannon-cast-photo.jpg') ?>" alt="Doran Hamm" class="credit-photo">
+          <h3 class="credit-name">Shannon Ward</h3>
+          <h3 class="credit-role">Prudence, Lisbetta</h3>
+        </div>
+      </div>
+      <div class="content-box tour-header">
+        <h1 class="tour-header">Special Thanks</h1>
+      </div>
+      <div class="program-section generic-credits-grid">
+        <div class="credit">
+          <h3 class="credit-role">Choreography</h3>
+          <h3 class="credit-name">Shoshana Bass</h3>
+        </div>
+        <div class="credit">
+          <h3 class="credit-role">Poster Design and Graphics</h3>
+          <h3 class="credit-name">Ross Smart</h3>
+        </div>
+      </div>
+      <div class="content-box tour-header">
+        <h1 class="tour-header">Our Sponsors</h1>
+      </div>
+      <div class="program-section">
+        <h2 class="tour-subtitle">Vermont Suitcase Company is proudly sponsored by:</h2>
+        <a href="https://foardpanel.com/" target="_blank"><img src="<?php echo get_theme_file_uri('/images/foardpanel-transp.png') ?>" alt="Foard Panel logo" class="sponsor-image"></a>
+      </div>
+      <div class="program-section">
+        <img src="<?php echo get_theme_file_uri('/images/whims-cast.jpg') ?>" alt="cast of My Lady of Whims" class="cast-photo">
+      </div>
+    </div>
+  </div>
+
+</div>
+
+
+
+
+
+
+
+
+<?php get_footer(); ?>
